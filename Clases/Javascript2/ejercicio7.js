5bdb331eb77d8109b060ff14c1f4b531b18b0ea8
Juan David <juanda7473@gmail.com>
2021-07-27 12:13:28 -0500

    ejercicio

---
+++ Clases/Javascript2/ejercicio7.js
@@ -0,0 +1,21 @@
+// ejercicio while
+
+let op = prompt("ingrese 1 para sumar y 2 para restar");
+
+while (op != "1" && op != "2") {
+    alert("Favor ingrese una opción valida entre 1 y 2");
+    let op = prompt("ingrese 1 para sumar y 2 para restar");
+}
+
+if (op != "1" && op != "2") {
+  alert("Favor ingrese una opción valida entre 1 y 2");
+} else {
+  alert("se piden los numeros y se hace operaciones");
+}
+
+
+//agregar el while y enviar las url de los dos trabajos 
+
+//agregar opción 5 para salir 
+//departamentos y capitales 
+//calculadora
