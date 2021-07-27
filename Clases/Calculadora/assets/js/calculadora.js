f2f9854ae9bd6bc6ea4330afcfb00e1d2e59d763
Juan David <juanda7473@gmail.com>
2021-07-27 11:00:48 -0500

    ajustes en el Js y carpeta assets

---
+++ Clases/Calculadora/assets/js/calculadora.js
@@ -0,0 +1,6 @@
+let op = prompt("ingrese 1 para sumar \n 2 para restar \n 3 para multiplicar \n 4 para dividir \n 5 para valor absoluto |x|");
+
+while (op != "1" && op != "2" && op != "3" && op != "4" && op != "5") {
+    alert("Favor ingrese una opción valida entre 1 al 5");
+    let op = prompt("ingrese 1 para sumar \n 2 para restar \n 3 para multiplicar \n 4 para dividir \n 5 para valor absoluto |x|");
+}
